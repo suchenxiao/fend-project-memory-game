77f77c707c86449794b70bd7fa38b96210b66e72
suchenxiao <suchenxiao@foxmail.com>
2018-02-18 15:05:47 +0800

    Feature: 实现结束页功能，游戏视觉效果基本实现
    存在bug2：点击过快会导致卡片匹配功能出错

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ var cardsHTML = [];
 $cards.each(function(i) {
 	cardsHTML[i] = $(this).html();
 });
+var $winPage = $('.win');
 
 restart();
 
@@ -43,6 +44,9 @@ function restart(){
     //重新计算步数
     $moves.text(0);
 	$stars.removeClass('fa-star-o');
+
+	//隐藏结束页
+	$winPage.css({'transform': 'scale(0,0)', 'opacity': 0});
 }
 
 //进行比对
@@ -86,7 +90,9 @@ function addMoves($obj){
 
 //游戏完成
 function gameWin($obj){
-    alert("Congratulations! With "+ parseInt($obj.text()) +" moves.");
+	var words = $winPage.children('p').first().text().replace('some', parseInt($obj.text())).replace('little', 3-$stars.filter('.fa-star-o').length);
+	$winPage.children('p').first().text( words );
+	$winPage.css({'transform': 'scale(1,1)', 'opacity': 1});
 }
 
 // 洗牌函数来自于 http://stackoverflow.com/a/2450976
@@ -103,18 +109,3 @@ function shuffle(array) {
 
     return array;
 }
-
-
-/*
- * 设置一张卡片的事件监听器。 如果该卡片被点击：
- *  - 显示卡片的符号（将这个功能放在你从这个函数中调用的另一个函数中）
- *  - 将卡片添加到状态为 “open” 的 *数组* 中（将这个功能放在你从这个函数中调用的另一个函数中）
- *  - 如果数组中已有另一张卡，请检查两张卡片是否匹配
- *    + 如果卡片匹配，将卡片锁定为 "open" 状态（将这个功能放在你从这个函数中调用的另一个函数中）
- *    + 如果卡片不匹配，请将卡片从数组中移除并隐藏卡片的符号（将这个功能放在你从这个函数中调用的另一个函数中）
- *    + 增加移动计数器并将其显示在页面上（将这个功能放在你从这个函数中调用的另一个函数中）
- *    + 如果所有卡都匹配，则显示带有最终分数的消息（将这个功能放在你从这个函数中调用的另一个函数中）
- */
-
-
-
